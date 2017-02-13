cc7bfdcc0f525f49231a415479ad822e0d47cdfe
lidahao <alwaysloseall@sina.com>
2017-02-13 18:01:47 +0800

    develop分支

--- view-editor/directives/widget.js
+++ view-editor/directives/widget.js
@@ -1,7 +1,20 @@
 ;(function(window, angular, undefined) { 'use strict';
 
 angular.module('biliSH')
-.directive('biWidget', ['$compile', function ($compile) {
-	
+//---文字
+.directive('biwText', ['$compile', function ($compile) {
+	return {
+		scope: {
+			'bindVar': '=', //【必须】绑定的变量
+		},
+		restrict: 'E',
+		template: '<div class="biw-text"></div>',
+		replace: true,
+		link: function (scope, ele) {
+			//---编译模板
+			var html = '<div>sss</div>';
+			ele.append($(html));
+		}
+	}
 }]);
 })(window, angular);
